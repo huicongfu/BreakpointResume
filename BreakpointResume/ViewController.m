48b0486c5816a36ed2f4f3f0d6a44d032551435b
huicongfu <347795319@qq.com>
2016-06-29 14:34:25 +0800

    details optimization

--- BreakpointResume/ViewController.m
+++ BreakpointResume/ViewController.m
@@ -47,15 +47,20 @@
 }
 - (IBAction)download:(UIButton *)sender {
     
-    sender.selected = ! sender.isSelected;
     
+     sender.selected = ! sender.isSelected;
     if (self.task == nil) {
+        [sender setTitle:@"暂停" forState:UIControlStateNormal];
         if (self.resumeData) {
             [self resume];
         }else
         {
             [self start];
         }
+    }else
+    {
+        [self pause];
+        [sender setTitle:@"点击下载" forState:UIControlStateNormal];
     }
 }
 
@@ -64,12 +69,17 @@
     /**
      *  开始下载的两个步骤
      */
-    //1.创建下载任务
-    NSURL * url = [NSURL URLWithString:@"http://filelx.liqucn.com/upload/2011/gps/baidumap_AndroidPhone"];
-    self.task = [self.session downloadTaskWithURL:url];
+    //1.创建下载任务 随便找的一个文件测试下载
+    NSURL * url = [NSURL URLWithString:@"http://xz6.jb51.net:81/201504/tools/zgdtu(jb51.net).rar"];
+    self.task = [[self session] downloadTaskWithURL:url];
     
     //2.开始下载任务
     [self.task resume];
+    
+    NSString * caches = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject];
+    //
+   
+    NSLog(@"路径：%@",caches);
 }
 
 /**
@@ -81,7 +91,7 @@
      *  继续下载的三步骤
      */
     
-    [[self session] downloadTaskWithResumeData:self.resumeData];
+    self.task = [[self session] downloadTaskWithResumeData:self.resumeData];
     [self.task resume];
     self.resumeData = nil;
 }
@@ -103,18 +113,28 @@
 #pragma mark - delegate
 - (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location
 {
-    UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"温馨提示：" message:@"文件下载好了哦！！！！" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"好的", nil];
+    UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"温馨提示：" message:@"文件下载好了哦！！！！" delegate:self cancelButtonTitle:nil otherButtonTitles:@"好的", nil];
     [alert show];
     
     //
     NSString * caches = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject];
     //
     NSString * file = [caches stringByAppendingPathComponent:downloadTask.response.suggestedFilename];
+    NSLog(@"路径：%@",file);
     NSFileManager * fileManager = [NSFileManager defaultManager];
     
-    //移动下载好的文件到指定的文件夹
-    [fileManager moveItemAtPath:location.path toPath:file error:nil];
     
+    if ([fileManager fileExistsAtPath:file]) {
+        NSLog(@"存放文件的路径已经存在该文件");
+        [fileManager removeItemAtPath:file error:nil];
+    }
+    
+    //移动下载好的文件到指定的文件夹
+    NSError * err = nil;
+    BOOL moveSuccess = [fileManager moveItemAtPath:location.path toPath:file error:&err];
+    
+    
+    NSLog(@"移动项目：%d,----错误原因：%@",moveSuccess,err);
     
 }
 
@@ -127,10 +147,10 @@
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
-    if (buttonIndex == 1) {
+    
         self.progressView.progress = 0.0f;
         self.progressLabel.text = @"当前没有下载任务";
-    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
